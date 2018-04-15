9f0fb1c753b15b18be742645b50eb9648af620fb
Yerlin Matu <yerlinmatu@gmail.com>
2018-04-15 03:46:36 -0500

    Files statics more api of movie json

--- server-app.go
+++ server-app.go
@@ -2,15 +2,28 @@ package main;
 
 import (
 	"net/http"
+	"encoding/json"
 	"fmt"
 );
+
+type Movie struct {
+	Name string `json:"name"`
+	Director string `json:"director"`
+	Netflix bool `json:"netflix"`
+}
 
 func main() {
 	localport := ":3000";
 	fmt.Println("Server run in port 3000");
+
 	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
 		http.ServeFile(res, req, "index.html");	
 	});
+
+	http.HandleFunc("/api", func(res http.ResponseWriter, req *http.Request) {
+		saga := Movie{"Black Mirror", "Charlie Brooker", true}
+		json.NewEncoder(res).Encode(saga);
+	})
 
 	http.ListenAndServe(localport, nil);
 }
